f930a2e03fa9acd25c2491395362f439fe642895
zak <mzhussain092@gmail.com>
2023-08-14 11:32:46 +0530

    Add quiz creation form

---
+++ app/quiz/page.tsx
@@ -0,0 +1,20 @@
+﻿import { getAuthSession } from "@/lib/nextAuth";
+import React from "react";
+import { redirect } from "next/navigation";
+import QuizCreation from "./components/QuizCreation";
+
+export const metadata = {
+  title: "Quiz | Kwizzing",
+  description: "Quiz",
+};
+
+type Props = {};
+
+const QuizPage = async (props: Props) => {
+  const session = await getAuthSession();
+
+  if (!session) redirect("/");
+  return <QuizCreation />;
+};
+
+export default QuizPage;
