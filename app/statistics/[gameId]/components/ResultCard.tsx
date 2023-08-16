95ccadc2858c784729016f776bf8070a0ea15660
zak <mzhussain092@gmail.com>
2023-08-16 17:51:01 +0530

    Add StatisticsPage UI

---
+++ app/statistics/[gameId]/components/ResultCard.tsx
@@ -0,0 +1,56 @@
+﻿import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Award, Trophy } from "lucide-react";
+import React from "react";
+
+type Props = {
+  accuracy: number;
+};
+
+const ResultCard = ({ accuracy }: Props) => {
+  return (
+    <Card className="md:col-span-7">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-7">
+        <CardTitle className="text-2xl font-bold">Results</CardTitle>
+        <Award />
+      </CardHeader>
+
+      <CardContent className="flex flex-col items-center justify-center h-3/5">
+        {accuracy > 75 && (
+          <>
+            <Trophy size={50} stroke="gold" className="" />
+            <div className="flex flex-col text-2xl font-semibold text-yellow-400 text-center">
+              <span>Impressive!</span>
+              <span className="text-center text-muted-foreground text-sm opacity-50">
+                You got {accuracy}% accurracy
+              </span>
+            </div>
+          </>
+        )}
+        {accuracy > 25 && accuracy <= 75 && (
+          <>
+            <Trophy size={50} stroke="silver" className="" />
+            <div className="flex flex-col text-2xl font-semibold text-slate-400 text-center">
+              <span>Great!</span>
+              <span className="text-center text-muted-foreground text-sm opacity-50">
+                You got {accuracy}% accurracy
+              </span>
+            </div>
+          </>
+        )}
+        {accuracy > 0 && accuracy <= 25 && (
+          <>
+            <Trophy size={50} stroke="red" className="" />
+            <div className="flex flex-col text-2xl font-semibold text-rose-600 text-center">
+              <span>Nice Try!</span>
+              <span className="text-center text-muted-foreground text-sm opacity-50">
+                You got {accuracy}% accurracy
+              </span>
+            </div>
+          </>
+        )}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ResultCard;
---
+++ app/statistics/[gameId]/page.tsx
@@ -0,0 +1,76 @@
+﻿import { buttonVariants } from "@/components/ui/button";
+import { getAuthSession } from "@/lib/nextAuth";
+import prismadb from "@/lib/prismadb";
+import { cn } from "@/lib/utils";
+import { CircleDashed, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import React from "react";
+import ResultCard from "./components/ResultCard";
+import AccuracyCard from "./components/AccuracyCard";
+import TimeTaken from "./components/TimeTaken";
+import QuestionsList from "./components/QuestionsList";
+
+type Props = {
+  params: {
+    gameId: string;
+  };
+};
+
+const StatisticsPage = async ({ params: { gameId } }: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) redirect("/");
+
+  const game = await prismadb.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: true,
+    },
+  });
+
+  if (!game) redirect("/quiz");
+
+  let accuracy = 0;
+  if (game.gameType === "mcq") {
+    let totalCorrect = game.questions.reduce((acc, question) => {
+      if (question.isCorrect) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    accuracy = Math.round((totalCorrect / game.questions.length) * 100);
+  } else if (game.gameType === "open_ended") {
+    let totalPercentage = game.questions.reduce((acc, question) => {
+      return acc + (question.percentageCorrect || 0);
+    }, 0);
+    accuracy = Math.round(totalPercentage / game.questions.length);
+  }
+  return (
+    <div className="p-8 mx-auto max-w-7xl">
+      <div className="flex item-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Statistics</h2>
+        <div className="flex items-center space-x-2">
+          <Link href={`/dashboard`} className={cn(buttonVariants())}>
+            <LayoutDashboard className="w-6 h-6 mr-2" /> Back to dashboard
+          </Link>
+        </div>
+      </div>
+
+      <div className="grid grid-cols-4 mt-4 md:grid-cols-7">
+        <ResultCard accuracy={accuracy} />
+        <AccuracyCard accuracy={accuracy} />
+        <TimeTaken
+          timeStarted={new Date()}
+          timeEnded={new Date(game.timeStarted ?? 0)}
+        />
+      </div>
+
+      <QuestionsList questions={game.questions} />
+    </div>
+  );
+};
+
+export default StatisticsPage;
